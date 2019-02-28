e331627e92d919e38bd35c77f13ca12695f98c20
yangjc <bupt_yjc@163.com>
2019-02-28 10:25:39 +0800

    change to trm

--- match.py
+++ match.py
@@ -13,29 +13,30 @@ device = torch.device('cpu')
 
 seq_len = 30
 
+bos, sep = '<', '-'
+
 path_word_ind = 'feat/word_ind.pkl'
 with open(path_word_ind, 'rb') as f:
     word_inds = pk.load(f)
 
-paths = {'esi': 'model/rnn_esi.pkl'}
+paths = {'trm': 'model/dnn_trm.pkl'}
 
-models = {'esi': torch.load(map_item('esi', paths), map_location=device)}
+models = {'trm': torch.load(map_item('trm', paths), map_location=device)}
 
 
 def predict(text1, text2, name):
     text1, text2 = clean(text1), clean(text2)
-    pad_seq1 = sent2ind(text1, word_inds, seq_len, keep_oov=True)
-    pad_seq2 = sent2ind(text2, word_inds, seq_len, keep_oov=True)
-    sent1 = torch.LongTensor([pad_seq1]).to(device)
-    sent2 = torch.LongTensor([pad_seq2]).to(device)
+    text = bos + text1 + sep + text2
+    pad_seq = sent2ind(text, word_inds, seq_len, keep_oov=True)
+    sent = torch.LongTensor([pad_seq]).to(device)
     model = map_item(name, models)
     with torch.no_grad():
         model.eval()
-        prob = torch.sigmoid(model(sent1, sent2))[0][0]
+        prob = torch.sigmoid(model(sent))[0][0]
     return '{:.3f}'.format(prob)
 
 
 if __name__ == '__main__':
     while True:
         text1, text2 = input('text1: '), input('text2: ')
-        print('esi: %s' % predict(text1, text2, 'esi'))
+        print('trm: %s' % predict(text1, text2, 'trm'))
